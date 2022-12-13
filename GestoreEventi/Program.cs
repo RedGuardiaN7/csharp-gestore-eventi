072e80e4d8b2201c62db2204fc7ffe142cfed2c6
RedGuardiaN7 <artiom.str@hotmail.it>
2022-12-13 09:56:35 +0100

    Add project files.

---
+++ GestoreEventi/Program.cs
@@ -0,0 +1,8 @@
+﻿//Consegna: Creare un gestore eventi per eventi come concerti, conferenze e spettacoli.
+//Senza interfaccia grafica, il gestore si deve occupare di:
+// - Memorizzare e tenere traccia di tutti gli eventi in futuro che ha programmato
+// - Poter gestire le prenotazioni e le disdette delle sue conferenze e tenere traccia
+//   quindi dei posti prenotati e di quelli disponibili per un dato evento
+// - Poter gestire un intero programma di Eventi (ossia tenere traccia di tutti gli eventi
+//   che afferiscono ad serie di Conferenze)
+
